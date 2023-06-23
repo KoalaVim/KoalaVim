6af8b61827a4a57d7e8564fd7b1e57bfe14864d7
Ofir Gal <ofir@gal.software>
2023-06-23 12:17:48 +0300

    caching sorter and finder

--- lua/KoalaVim/utils/session.lua
+++ lua/KoalaVim/utils/session.lua
@@ -1,11 +1,13 @@
 local M = {}
 
-function M.list_sessions()
+local path_utils = require('KoalaVim.utils.path')
+
+local function get_session_sorter()
 	local fzy_sorter = require('telescope.sorters').get_fzy_sorter()
-	local path_utils = require('KoalaVim.utils.path')
 
 	local current_cwd_session_pattern = '^' .. vim.fn.getcwd()
-	local session_sorter = require('telescope.sorters').Sorter:new({
+
+	return require('telescope.sorters').Sorter:new({
 		scoring_function = function(a, prompt, line)
 			local fzy_score = fzy_sorter.scoring_function(a, prompt, line)
 			if fzy_score < 0 then
@@ -21,22 +23,30 @@ function M.list_sessions()
 		discard = true,
 		highlighter = fzy_sorter.highlighter,
 	})
+end
 
+local function get_session_finder()
+	print('get_session_finder')
 	local home_dir_regex = '^' .. vim.loop.os_homedir()
-	local get_session_finder = function()
-		local sessions = require('possession.query').as_list()
-		return require('telescope.finders').new_table({
-			results = sessions,
-			entry_maker = function(entry)
-				local unescaped_name = path_utils.unescape_dir(entry.name)
-				return {
-					value = entry,
-					display = unescaped_name:gsub(home_dir_regex, '~'),
-					ordinal = unescaped_name,
-				}
-			end,
-		})
-	end
+	local sessions = require('possession.query').as_list()
+
+	return require('telescope.finders').new_table({
+		results = sessions,
+		entry_maker = function(entry)
+			local unescaped_name = path_utils.unescape_dir(entry.name)
+			return {
+				value = entry,
+				display = unescaped_name:gsub(home_dir_regex, '~'),
+				ordinal = unescaped_name,
+			}
+		end,
+	})
+end
+
+function M.list_sessions()
+	-- Cache sorter and finder
+	M._sorter = M._sorter or get_session_sorter()
+	M._finder = M._finder or get_session_finder()
 
 	require('telescope').extensions.possession.list({
 		prompt_title = 'Choose Session (sorted by cwd and frequency)',
@@ -45,8 +55,8 @@ function M.list_sessions()
 			height = 0.30,
 			width = 0.40,
 		},
-		sorter = session_sorter,
-		finder = get_session_finder(),
+		sorter = M._sorter,
+		finder = M._finder,
 		layout_strategy = 'center',
 		sorting_strategy = 'ascending', -- From top
 	})
