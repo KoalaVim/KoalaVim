f3f2eb1ea9975a5b4138ea10aea12ae6c4d66c0e
Ofir Gal <ofir@gal.software>
2023-07-13 12:13:38 +0300

    Fixed <F3> in insert mode..

--- lua/KoalaVim/config/keymaps/search.lua
+++ lua/KoalaVim/config/keymaps/search.lua
@@ -4,6 +4,6 @@ local map = require('KoalaVim.utils.map').map
 map('n', 'n', 'nzz', 'Auto recenter after n')
 map('n', 'N', 'Nzz', 'Auto recenter after N')
 map('n', '<F3>', '<cmd>let @/ = "not_gonna_find_this_______"<cr>', 'Disable find highlight')
-map('i', '<F3>', '<Esc><cmd>let @/ = "not_gonna_find_this_______"<cr>i', 'Disable find highlight')
+map('i', '<F3>', '<Esc><cmd>let @/ = "not_gonna_find_this_______"<cr>a', 'Disable find highlight')
 
 map('n', '*', "<cmd>let @/= '\\<' . expand('<cword>') . '\\>'<cr>zz", 'Search current word without jump')
