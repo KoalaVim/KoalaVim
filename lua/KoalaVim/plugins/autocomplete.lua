98d24a16f7462f9621a10697b70ec721f2f73fc7
Ofir Gal <ofir@gal.software>
2023-08-21 13:26:49 +0300

    remove nvim_lsp_signature_help entry from cmp (cmp-nvim-lsp-signature-help has been removed)

--- lua/KoalaVim/plugins/autocomplete.lua
+++ lua/KoalaVim/plugins/autocomplete.lua
@@ -130,7 +130,6 @@ table.insert(M, {
 			},
 			window = require('ofirkai.plugins.nvim-cmp').window,
 			sources = cmp.config.sources({
-				{ name = 'nvim_lsp_signature_help', priority = 1500 },
 				{ name = 'nvim_lsp', priority = 1000 },
 				{ name = 'path', option = { trailing_slash = true }, priority = 500 },
 				{ name = 'snippy', priority = 200 },
