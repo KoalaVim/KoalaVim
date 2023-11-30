4d290061cee730e491ebf73ecb35af28d9c11192
Ofir Gal <ofir@gal.software>
2023-11-30 17:51:51 +0200

    mypy imports errors are warnings

--- lua/KoalaVim/plugins/lsp/servers/python.lua
+++ lua/KoalaVim/plugins/lsp/servers/python.lua
@@ -38,6 +38,15 @@ NONE_LS_SRCS['mypy'] = {
 				require('KoalaVim.consts').null_ls.methods.DIAGNOSTICS_ON_OPEN,
 				require('KoalaVim.consts').null_ls.methods.DIAGNOSTICS_ON_SAVE,
 			},
+			diagnostics_postprocess = function(diagnostic)
+				if
+					diagnostic.code == 'import-not-found'
+					or diagnostic.message
+						== 'See https://mypy.readthedocs.io/en/stable/running_mypy.html#missing-imports'
+				then
+					diagnostic.severity = vim.diagnostic.severity['WARN']
+				end
+			end,
 		},
 	},
 }
