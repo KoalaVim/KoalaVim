0244a29e796b784ec2937f46625b7ae2337bf113
Amit Tamari <15573913+amittamari@users.noreply.github.com>
2023-12-29 17:09:54 +0200

    add `LazyRestoreLogged` usercmd

---
+++ lua/KoalaVim/utils/restore.lua
@@ -0,0 +1,22 @@
+local M = {}
+
+function M.restore_logged()
+	local res = require('lazy').restore()
+	res:wait()
+
+	local plugins = res['_opts']['plugins']
+	local ret = { plugins = {} }
+
+	for _, plugin in ipairs(plugins) do
+		local tasks = plugin['_']['tasks']
+		for _, task in ipairs(tasks) do
+			if task.error then
+				ret.plugins[plugin[1]] = task.error
+			end
+		end
+	end
+
+	print(vim.json.encode(ret))
+end
+
+return M
