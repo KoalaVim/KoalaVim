38f3f05b73d74e65c5ebd0e0f13bf3d2ad842c20
Ofir Gal <ofirgal753@gmail.com>
2024-01-13 13:33:34 +0200

    KoalaUpdate: added progress with fidget

--- lua/KoalaVim/utils/update_checker.lua
+++ lua/KoalaVim/utils/update_checker.lua
@@ -1,11 +1,13 @@
 local M = {}
 
 local api = vim.api
-local uv = vim.loop
 
 local kstate = require('KoalaVim').state
 local kstate_manager = require('KoalaVim.state')
-local UPDATE_FETCH_INTERVAL = 30 * 60 -- 30 mins
+local UPDATE_FETCH_INTERVAL = 60 * 5 -- 5 mins
+local PROGRESS = nil
+
+local count = 0
 
 -- Using lazy modules as lib
 local Git = require('lazy.manage.git')
@@ -32,6 +34,8 @@ local function _check_local_git()
 	end
 
 	if Git.eq(curr, target) then
+		M.render('bdika ' .. count, false)
+		count = count + 1
 		return nil -- No updates
 	end
 
@@ -62,36 +66,55 @@ local function _on_lazy_check_done()
 	-- update state
 	kstate.last_update_check = os.time()
 	kstate_manager.save()
+	if PROGRESS then
+		PROGRESS:finish()
+	end
 	_check_local_git()
 end
 
 local function _check()
 	local last_update_check = kstate.last_update_check or 0
-	if os.time() - last_update_check < UPDATE_FETCH_INTERVAL then
+	if os.difftime(os.time(), last_update_check) < UPDATE_FETCH_INTERVAL then
 		-- No need to fetch
 		_check_local_git()
 		return
 	end
 
+	PROGRESS = require('fidget.progress').handle.create({
+		title = 'Checking for updates',
+		lsp_client = { name = 'KoalaVim' },
+		percentage = 0,
+	})
+
+	_check_local_git()
+
 	-- Leverage lazy to fetch koala updates
 	local res = require('lazy').check({ plugins = { 'KoalaVim' }, show = false })
+	-- Show local results until lazy check is done
 	res:wait(_on_lazy_check_done)
 end
 
 local WIN_WIDTH = 65
 local BASE_HEIGHT = 3
 local MAX_COMMITS = 15
+local BUF = nil
+local WIN = nil
 
 function M.render(message, error)
 	if message == '' then
 		return -- No updates to show. local git is ahead of remote probably
 	end
 
-	local buf = api.nvim_create_buf(false, true)
-	api.nvim_buf_set_option(buf, 'filetype', 'KoalaUpdates')
+	if BUF then
+		-- Clear buffer
+		api.nvim_buf_set_lines(BUF, 1, -1, false, {})
+	else
+		BUF = api.nvim_create_buf(false, true)
+	end
+	api.nvim_buf_set_option(BUF, 'filetype', 'KoalaUpdates')
 	local ns = api.nvim_create_namespace('KoalaUpdates')
 
-	api.nvim_buf_clear_namespace(buf, ns, 0, -1)
+	api.nvim_buf_clear_namespace(BUF, ns, 0, -1)
 
 	local title = 'New Koala Updates'
 	local title_hl = 'Title'
@@ -114,7 +137,7 @@ function M.render(message, error)
 		spacer = spacer .. '─'
 	end
 
-	api.nvim_buf_set_extmark(buf, ns, 0, 0, {
+	api.nvim_buf_set_extmark(BUF, ns, 0, 0, {
 		virt_text_pos = 'overlay',
 		virt_text = { { title, title_hl } },
 		virt_lines = { { { spacer, 'FloatBorder' } } },
@@ -128,32 +151,34 @@ function M.render(message, error)
 		end
 	end
 
-	api.nvim_buf_set_lines(buf, 2, -1, false, lines)
+	api.nvim_buf_set_lines(BUF, 2, -1, false, lines)
 
-	local win = api.nvim_open_win(buf, false, {
-		relative = 'editor',
-		width = WIN_WIDTH,
-		col = 1,
-		row = 1,
-		style = 'minimal',
-		height = BASE_HEIGHT + #lines,
-		border = 'rounded',
-	})
+	if not WIN then
+		WIN = api.nvim_open_win(BUF, false, {
+			relative = 'editor',
+			width = WIN_WIDTH,
+			col = 1,
+			row = 1,
+			style = 'minimal',
+			height = BASE_HEIGHT + #lines,
+			border = 'rounded',
+		})
 
-	api.nvim_set_option_value('winhighlight', 'Normal:Normal,FloatBorder:' .. border_hl, { win = win })
+		api.nvim_set_option_value('winhighlight', 'Normal:Normal,FloatBorder:' .. border_hl, { win = WIN })
 
-	-- Clean pop up after alpha closed
-	api.nvim_create_autocmd('User', {
-		pattern = 'AlphaClosed',
-		callback = function()
-			if api.nvim_buf_is_valid(win) then
-				api.nvim_win_close(win, true)
-			end
-			if api.nvim_buf_is_valid(buf) then
-				api.nvim_buf_delete(buf, { force = true })
-			end
-		end,
-	})
+		-- Clean pop up after alpha closed
+		api.nvim_create_autocmd('User', {
+			pattern = 'AlphaClosed',
+			callback = function()
+				if api.nvim_buf_is_valid(WIN) then
+					api.nvim_win_close(WIN, true)
+				end
+				if api.nvim_buf_is_valid(BUF) then
+					api.nvim_buf_delete(BUF, { force = true })
+				end
+			end,
+		})
+	end
 end
 
 function M.check()
