b30b9917a4c698379e679c6f2687b85d785059db
Ofir Gal <ofirgal753@gmail.com>
2024-03-09 12:21:43 +0200

    kv: restart nvim indicator moved to data path

--- lua/KoalaVim/utils/misc.lua
+++ lua/KoalaVim/utils/misc.lua
@@ -7,7 +7,10 @@ function M.center_screen()
 end
 
 function M.restart_nvim()
-	vim.fn.system('touch /tmp/restart_nvim')
+	local f = io.open(vim.fn.stdpath('data') .. '/restart_kvim', 'w')
+	if f ~= nil then
+		f:close()
+	end
 	api.nvim_feedkeys(':wqa\n', 'n', false)
 end
 
