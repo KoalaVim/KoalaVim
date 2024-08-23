8e6a5ab9f0fb5591f7c09b787ccb0b2a457d948d
Ofir Gal <ofirgal753@gmail.com>
2024-08-23 19:08:14 +0300

    changed treesitter move to function bind

--- lua/KoalaVim/plugins/treesitter.lua
+++ lua/KoalaVim/plugins/treesitter.lua
@@ -39,29 +39,27 @@ table.insert(M, {
 				enable = true,
 				set_jumps = true, -- whether to set jumps in the jumplist
 				goto_next_start = {
-					[']m'] = '@function.outer',
+					[']f'] = '@function.outer',
 					[']]'] = '@class.outer',
 					[']b'] = '@block.outer',
 					[']a'] = '@parameter.inner',
 					[']k'] = '@call.outer',
 				},
 				goto_next_end = {
-					[']M'] = '@function.outer',
-					[']['] = '@class.outer',
+					[']F'] = '@function.outer',
 					[']B'] = '@block.outer',
 					[']A'] = '@parameter.inner',
 					[']K'] = '@call.outer',
 				},
 				goto_previous_start = {
-					['[m'] = '@function.outer',
+					['[f'] = '@function.outer',
 					['[['] = '@class.outer',
 					['[b'] = '@block.outer',
 					['[a'] = '@parameter.inner',
 					['[k'] = '@call.inner',
 				},
 				goto_previous_end = {
-					['[M'] = '@function.outer',
-					['[]'] = '@class.outer',
+					['[F'] = '@function.outer',
 					['[B'] = '@block.outer',
 					['[A'] = '@parameter.inner',
 					['[K'] = '@call.inner',
