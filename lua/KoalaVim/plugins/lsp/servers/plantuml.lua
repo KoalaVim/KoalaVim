58b687294967966b40366854137f6b4c429d2ba3
Ofir Gal <ofirgal753@gmail.com>
2024-09-04 11:47:29 +0300

    plantuml: add syntax highlight, add usercmd for generating UML image

--- lua/KoalaVim/plugins/lsp/servers/plantuml.lua
+++ lua/KoalaVim/plugins/lsp/servers/plantuml.lua
@@ -1,10 +1,34 @@
 local M = {}
 
 table.insert(M, {
-	'https://gitlab.com/itaranto/plantuml.nvim',
-	opts = {},
+	'aklt/plantuml-syntax',
+})
+
+table.insert(M, {
+	'ofirgall/plantuml.nvim', -- fork
+	ft = {
+		'iuml',
+		'plantuml',
+		'pu',
+		'puml',
+		'wsd',
+	},
+	opts = {
+		image_renderer = { -- for KoalaVim
+			type = 'image',
+			options = {
+				prog = 'feh',
+				dark_mode = false,
+			},
+		},
+	},
 	config = function(_, opts)
 		require('plantuml').setup(opts)
+
+		local image_renderer = require('plantuml').create_renderer(opts.image_renderer)
+		require('KoalaVim.utils.cmd').create('UML', 'Render plantuml as image', function()
+			require('plantuml').render_file(image_renderer, vim.api.nvim_buf_get_name(0))
+		end)
 	end,
 })
 
