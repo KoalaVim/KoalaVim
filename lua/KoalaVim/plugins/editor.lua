3ac85a85c0950416f92a7c98d8f1879c48168e18
Ofir Gal <ofirgal753@gmail.com>
2024-09-07 20:19:06 +0300

    bracket-repeat renamed to bracket-repeat.nvim

--- lua/KoalaVim/plugins/editor.lua
+++ lua/KoalaVim/plugins/editor.lua
@@ -675,7 +675,7 @@ table.insert(M, {
 })
 
 table.insert(M, {
-	'ofirgall/bracket-repeat',
+	'ofirgall/bracket-repeat.nvim',
 	priority = 99999999999, -- load last
 	config = function(_, opts)
 		require('bracket-repeat').setup(opts)
