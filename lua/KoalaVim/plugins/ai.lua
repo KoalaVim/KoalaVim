383c140f3115517937e95e121be03db7327c8aa1
Ofir Gal <ofirgal753@gmail.com>
2026-02-02 19:32:02 +0200

    add basic integration for sidekick.nvim

--- lua/KoalaVim/plugins/ai.lua
+++ lua/KoalaVim/plugins/ai.lua
@@ -23,4 +23,83 @@ table.insert(M, {
 	end,
 })
 
+table.insert(M, {
+	'folke/sidekick.nvim',
+	keys = {
+		{
+			'<leader>uN',
+			function()
+				require('sidekick.nes').toggle()
+			end,
+			desc = 'Toggle sidekick NES',
+		},
+		-- nes is also useful in normal mode
+		-- FIXME: enable 'nes'
+		-- { '<tab>', LazyVim.cmp.map({ 'ai_nes' }, '<tab>'), mode = { 'n' }, expr = true },
+		{ '<leader>a', '', desc = '+ai', mode = { 'n', 'v' } },
+		{
+			'<c-.>',
+			function()
+				require('sidekick.cli').toggle()
+			end,
+			desc = 'Sidekick Toggle',
+			mode = { 'n', 't', 'i', 'x' },
+		},
+		{
+			'<leader>aa',
+			function()
+				require('sidekick.cli').toggle()
+			end,
+			desc = 'Sidekick Toggle CLI',
+		},
+		{
+			'<leader>as',
+			function()
+				require('sidekick.cli').select()
+			end,
+			-- Or to select only installed tools:
+			-- require("sidekick.cli").select({ filter = { installed = true } })
+			desc = 'Select CLI',
+		},
+		{
+			'<leader>ad',
+			function()
+				require('sidekick.cli').close()
+			end,
+			desc = 'Detach a CLI Session',
+		},
+		{
+			'<leader>at',
+			function()
+				require('sidekick.cli').send({ msg = '{this}' })
+			end,
+			mode = { 'x', 'n' },
+			desc = 'Send This',
+		},
+		{
+			'<leader>af',
+			function()
+				require('sidekick.cli').send({ msg = '{file}' })
+			end,
+			desc = 'Send File',
+		},
+		{
+			'<leader>av',
+			function()
+				require('sidekick.cli').send({ msg = '{selection}' })
+			end,
+			mode = { 'x' },
+			desc = 'Send Visual Selection',
+		},
+		{
+			'<leader>ap',
+			function()
+				require('sidekick.cli').prompt()
+			end,
+			mode = { 'n', 'x' },
+			desc = 'Sidekick Select Prompt',
+		},
+	},
+})
+
 return M
