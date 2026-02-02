4512e758f95c0f005e1c547ed5c81780efaddbe0
Ofir Gal <ofirgal753@gmail.com>
2026-02-02 21:09:49 +0200

    rust: fixed lsp installation. replaced rust-tools.nvim in favor of rustaceanvim

--- lua/KoalaVim/plugins/lsp/servers/rust.lua
+++ lua/KoalaVim/plugins/lsp/servers/rust.lua
@@ -2,45 +2,84 @@ local M = {}
 
 -- for some reason format of rust-analyzer + conform + format-on-leave doesn't work.
 -- using rustfmt through conform solves the problem.
-CONFORM_FORMATTERS['rustfmt'] = { 'rust' }
+-- CONFORM_FORMATTERS['rustfmt'] = { 'rust' }
+
+local map = require('KoalaVim.utils.map')
+
+LSP_SERVERS['rust-analyzer'] = {
+	dont_setup = true, -- Dont setup but install with mason
+	mason = 'rust_analyzer',
+}
 
 table.insert(M, {
-	'simrat39/rust-tools.nvim',
+	'mrcjkb/rustaceanvim',
 	ft = 'rust',
-	config = function()
-		require('rust-tools').setup({
-			server = {
-				on_attach = LSP_ON_ATTACH,
-				on_init = LSP_ON_INIT,
-				capabilities = LSP_CAPS,
-				settings = {
-					['rust-analyzer'] = {
-						completion = { callable = { snippets = 'add_parentheses' } },
+	opts = {
+		server = {
+			on_attach = function(_, bufnr)
+				map.map_buffer(bufnr, 'n', '<F4>', function()
+					vim.cmd.RustLsp('codeAction')
+				end, 'Rust Code Action')
+
+				-- override hover
+				map.map_buffer(bufnr, 'n', 'K', function()
+					vim.cmd.RustLsp({ 'hover', 'actions' })
+				end, 'Rust Hover')
+
+				map.map_buffer(bufnr, 'n', 'J', function()
+					vim.cmd.RustLsp('joinLines')
+				end, 'Rust Join Lines')
+
+				map.map_buffer(bufnr, 'n', '<leader>rr', function()
+					vim.cmd.RustLsp('runnables')
+				end, 'Rust Runnables')
+
+				map.map_buffer(bufnr, 'n', '<leader>rt', function()
+					vim.cmd.RustLsp('testables')
+				end, 'Rust Testables')
+			end,
+			default_settings = {
+				-- rust-analyzer language server configuration
+				['rust-analyzer'] = {
+					cargo = {
+						allFeatures = true,
+						loadOutDirsFromCheck = true,
+						buildScripts = {
+							enable = true,
+						},
+					},
+					-- FIXME: add bacon from LazyVim
+					-- Add clippy lints for Rust if using rust-analyzer
+					checkOnSave = 'rust-analyzer',
+					-- Enable diagnostics if using rust-analyzer
+					diagnostics = {
+						enable = 'rust-analyzer',
+					},
+					procMacro = {
+						enable = true,
+					},
+					files = {
+						exclude = {
+							'.direnv',
+							'.git',
+							'.jj',
+							'.github',
+							'.gitlab',
+							'bin',
+							'node_modules',
+							'target',
+							'venv',
+							'.venv',
+						},
+						-- Avoid Roots Scanned hanging, see https://github.com/rust-lang/rust-analyzer/issues/12613#issuecomment-2096386344
+						watcher = 'client',
 					},
 				},
 			},
-			tools = {
-				reload_workspace_from_cargo_toml = false,
-				inlay_hints = {
-					auto = false, -- Using inlay-hints.nvim instead
-				},
-				hover_actions = {
-					auto_focus = true,
-				},
-			},
-		})
-
-		local map_buffer = require('KoalaVim.utils.map').map_buffer
-		vim.api.nvim_create_autocmd('FileType', {
-			pattern = 'rust',
-			callback = function(events)
-				local bufid = events.buf
-				map_buffer(bufid, 'n', 'J', require('rust-tools').join_lines.join_lines, 'Rust: join line')
-				map_buffer(bufid, 'n', '<leader>rr', require('rust-tools').runnables.runnables, 'Rust: run')
-				-- stylua: ignore
-				map_buffer( bufid, 'n', '<leader>rt', require('rust-tools').hover_actions.hover_actions, 'Rust: run test') -- run test
-			end,
-		})
+		},
+	},
+	config = function(_, opts)
+		vim.g.rustaceanvim = vim.tbl_deep_extend('keep', vim.g.rustaceanvim or {}, opts or {})
 	end,
 })
 
