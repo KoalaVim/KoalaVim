e5df3b007361381732eed0a4eb6cc96940a65784
Ofir Gal <ofirgal753@gmail.com>
2026-02-09 23:19:47 +0200

    restart_nvim: close sidekick before restarting

--- lua/KoalaVim/utils/misc.lua
+++ lua/KoalaVim/utils/misc.lua
@@ -11,6 +11,17 @@ function M.restart_nvim()
 	if f ~= nil then
 		f:close()
 	end
+
+	-- FIXME: move to possession.nvim
+	local sidekick_ok, sidekick_cli = pcall(require, 'sidekick.cli')
+	if sidekick_ok then
+		sidekick_cli.close()
+		vim.schedule(function()
+			api.nvim_feedkeys(':wqa\n', 'n', false)
+		end)
+		return
+	end
+
 	api.nvim_feedkeys(':wqa\n', 'n', false)
 end
 
