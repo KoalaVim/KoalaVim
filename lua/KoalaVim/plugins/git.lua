2a05830f69e35dd86e5f103e6f49907870b3b0dc
Ofir Gal <ofirgal753@gmail.com>
2026-02-11 16:09:03 +0200

    configured octo.nvim

--- lua/KoalaVim/plugins/git.lua
+++ lua/KoalaVim/plugins/git.lua
@@ -557,6 +557,35 @@ table.insert(M, {
 table.insert(M, {
 	'pwntester/octo.nvim',
 	cmd = 'Octo',
+	event = { { event = 'BufReadCmd', pattern = 'octo://*' } },
+	opts = {
+		enable_builtin = true,
+		default_to_projects_v2 = false,
+		default_merge_method = 'squash',
+		picker = 'telescope',
+	},
+	keys = {
+		{ '<leader>gil', '<cmd>Octo issue list<CR>', desc = 'List Issues (Octo)' },
+		{ '<leader>gic', '<cmd>Octo issue create<CR>', desc = 'List Issues (Octo)' },
+		{ '<leader>gI', '<cmd>Octo issue search<CR>', desc = 'Search Issues (Octo)' },
+		{ '<leader>gr', '<cmd>Octo pr list<CR>', desc = 'List PRs (Octo)' },
+		{ '<leader>gR', '<cmd>Octo pr search<CR>', desc = 'Search PRs (Octo)' },
+		{ '<leader>gl', '<cmd>Octo repo list<CR>', desc = 'List Repos (Octo)' },
+		{ '<leader>gS', '<cmd>Octo search<CR>', desc = 'Search (Octo)' },
+
+		{ '<localleader>a', '', desc = '+assignee (Octo)', ft = 'octo' },
+		{ '<localleader>c', '', desc = '+comment/code (Octo)', ft = 'octo' },
+		{ '<localleader>l', '', desc = '+label (Octo)', ft = 'octo' },
+		{ '<localleader>i', '', desc = '+issue (Octo)', ft = 'octo' },
+		{ '<localleader>r', '', desc = '+react (Octo)', ft = 'octo' },
+		{ '<localleader>p', '', desc = '+pr (Octo)', ft = 'octo' },
+		{ '<localleader>pr', '', desc = '+rebase (Octo)', ft = 'octo' },
+		{ '<localleader>ps', '', desc = '+squash (Octo)', ft = 'octo' },
+		{ '<localleader>v', '', desc = '+review (Octo)', ft = 'octo' },
+		{ '<localleader>g', '', desc = '+goto_issue (Octo)', ft = 'octo' },
+		{ '@', '@<C-x><C-o>', mode = 'i', ft = 'octo', silent = true },
+		{ '#', '#<C-x><C-o>', mode = 'i', ft = 'octo', silent = true },
+	},
 	config = function(_, opts)
 		require('octo').setup(opts)
 	end,
