e0874750db15c37dc89ae14dc963dfbc8a57db3c
Ofir Gal <ofirgal753@gmail.com>
2026-02-13 19:37:00 +0200

    add nerdy.nvim

--- lua/KoalaVim/plugins/editor.lua
+++ lua/KoalaVim/plugins/editor.lua
@@ -703,4 +703,21 @@ table.insert(M, {
 	},
 })
 
+table.insert(M, {
+	'2kabhishek/nerdy.nvim',
+	dependencies = {
+		'folke/snacks.nvim',
+	},
+	cmd = 'Nerdy',
+	opts = {
+		max_recents = 30, -- Configure recent icons limit
+		copy_to_clipboard = false, -- Copy glyph to clipboard instead of inserting
+		copy_register = '+', -- Register to use for copying (if `copy_to_clipboard` is true)
+	},
+	keys = {
+		{ '<leader>fi', '<cmd>Nerdy list<CR>', desc = 'Browse nerd icons' },
+		{ '<leader>fI', '<cmd>Nerdy recents<CR>', desc = 'Browse recent nerd icons' },
+	},
+})
+
 return M
