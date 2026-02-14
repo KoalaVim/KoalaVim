1b6d8a3eaa7be9ddeed283e3dab7132f53b58061
Ofir Gal <ofirgal753@gmail.com>
2026-02-15 00:49:16 +0200

    add smear-cursor

--- lua/KoalaVim/plugins/ui.lua
+++ lua/KoalaVim/plugins/ui.lua
@@ -683,4 +683,31 @@ table.insert(M, {
 	end,
 })
 
+table.insert(M, {
+	'sphamba/smear-cursor.nvim',
+	event = 'VeryLazy',
+	cond = vim.g.neovide == nil,
+	opts = {
+		-- stylua: ignore start               -- Default  Range
+		-- stiffness = 0.8,                      -- 0.6      [0, 1]
+		-- trailing_stiffness = 0.6,             -- 0.45     [0, 1]
+		-- stiffness_insert_mode = 0.7,          -- 0.5      [0, 1]
+		-- trailing_stiffness_insert_mode = 0.7, -- 0.5      [0, 1]
+		-- damping = 0.95,                       -- 0.85     [0, 1]
+		-- damping_insert_mode = 0.95,           -- 0.9      [0, 1]
+		-- distance_stop_animating = 0.5,        -- 0.1      > 0
+		-- stylua: ignore end
+
+		-- Fixes issues with blinking chars after animating
+		hide_target_hack = true,
+		never_draw_over_target = true,
+
+		cursor_color = 'none',
+		legacy_computing_symbols_support = true,
+		smear_insert_mode = false, -- no effect?
+	},
+	specs = {
+	},
+})
+
 return M
