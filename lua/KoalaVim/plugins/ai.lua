4aa1c9fd33150e3a704b82e93d2f4728e0466319
Ofir Gal <ofirgal753@gmail.com>
2026-04-13 11:22:46 +0300

    sidekick: C-g for git status, C-d for git diff

--- lua/KoalaVim/plugins/ai.lua
+++ lua/KoalaVim/plugins/ai.lua
@@ -67,12 +67,21 @@ table.insert(M, {
 	},
 	keys = {
 		{
-			'<C-g>',
+			'<C-d>',
 			function()
 				require('KoalaVim.utils.git').show_diff()
 			end,
 			ft = 'sidekick_terminal',
 			desc = 'Open Diff',
+			mode = { 'n', 't' },
+		},
+		{
+			'<C-g>',
+			function()
+				require('KoalaVim.utils.git').show_status()
+			end,
+			ft = 'sidekick_terminal',
+			desc = 'Open Git Status',
 			mode = { 'n', 't' },
 		},
 		{
