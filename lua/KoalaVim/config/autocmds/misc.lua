833f64320f9a66b5e0e3598e5698e3828cec3810
Ofir Gal <ofirgal753@gmail.com>
2026-04-23 22:06:07 +0300

    ai: quit nvim when closing last sidekick terminal

--- lua/KoalaVim/config/autocmds/misc.lua
+++ lua/KoalaVim/config/autocmds/misc.lua
@@ -11,6 +11,39 @@ api.nvim_create_autocmd('FileType', {
 	end,
 })
 
+api.nvim_create_autocmd('BufUnload', {
+	group = koala_early_autocmds,
+	callback = function(args)
+		if vim.bo[args.buf].filetype ~= 'sidekick_terminal' then
+			return
+		end
+		vim.schedule(function()
+			local non_floating_tabs = 0
+			for _, t in ipairs(api.nvim_list_tabpages()) do
+				for _, w in ipairs(api.nvim_tabpage_list_wins(t)) do
+					if api.nvim_win_get_config(w).relative == '' then
+						non_floating_tabs = non_floating_tabs + 1
+						break
+					end
+				end
+			end
+			if non_floating_tabs > 1 then
+				return
+			end
+			for _, b in ipairs(api.nvim_list_bufs()) do
+				if b ~= args.buf and vim.bo[b].buflisted then
+					local name = api.nvim_buf_get_name(b)
+					local modified = vim.bo[b].modified
+					if name ~= '' or modified then
+						return
+					end
+				end
+			end
+			vim.cmd('qa!')
+		end)
+	end,
+})
+
 vim.filetype.add({
 	extension = {
 		mdc = 'markdown',
